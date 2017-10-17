b430c383a09c83d7e478511feaaafccedecc5cc8
Ricardo Machado <rmachado@travix.com>
2017-10-17 13:11:36 +0200

    Changes the preset to bring the needed plugins + stage 3 instead of stage 0

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const moduleExportsPlugin = require('babel-plugin-add-module-exports');
 const babelPresetEs2015 = require('babel-preset-es2015');
-const babelPresetStage0 = require('babel-preset-stage-0');
+const babelPresetStage3 = require('babel-preset-stage-3');
 const babelPresetReact = require('babel-preset-react');
+const babelPluginTransformExportDefault = require('babel-plugin-transform-export-default');
 const babelPluginTransformEs2015ObjectSuper = require('babel-plugin-transform-es2015-object-super');
+const babelPluginTransformFunctionBind = require('babel-plugin-transform-function-bind');
 
 module.exports = {
   presets: [
     babelPresetEs2015,
-    babelPresetStage0,
+    babelPresetStage3,
     babelPresetReact,
   ],
   plugins: [
     moduleExportsPlugin.default ? moduleExportsPlugin.default : moduleExportsPlugin,
     babelPluginTransformEs2015ObjectSuper,
+    babelPluginTransformExportDefault,
+    babelPluginTransformFunctionBind,
   ],
 };
