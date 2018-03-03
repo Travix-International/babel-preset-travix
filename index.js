b7ff7cc93a3a46c7c84b0037fbc23ef3f1864246
leandrooriente <leandroriente@gmail.com>
2018-03-03 18:12:42 +0100

    Update babel-preset-es2015 to babel-preset-env

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const moduleExportsPlugin = require('babel-plugin-add-module-exports');
-const babelPresetEs2015 = require('babel-preset-es2015');
+const babelPresetEnv = require('babel-preset-env');
 const babelPresetStage3 = require('babel-preset-stage-3');
 const babelPresetReact = require('babel-preset-react');
 const babelPluginTransformExportDefault = require('babel-plugin-transform-export-default');
 const babelPluginTransformEs2015ObjectSuper = require('babel-plugin-transform-es2015-object-super');
 const babelPluginTransformFunctionBind = require('babel-plugin-transform-function-bind');
 
+const targets = {
+  node: 'current',
+  android: 30,
+  chrome: 50,
+  edge: 14,
+  explorer: 11,
+  firefox: 52,
+  safari: 9,
+  ucandroid: 1
+};
+
 module.exports = {
   presets: [
-    babelPresetEs2015,
+    babelPresetEnv.default(null, { targets }),
     babelPresetStage3,
     babelPresetReact,
   ],
