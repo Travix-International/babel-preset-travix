712a37714c19b710bc179c10ef904e60ebde1296
Leandro Oriente <leandroriente@gmail.com>
2018-03-22 11:36:37 +0100

    Update babel-preset-es2015 to babel-preset-env (#27)
    * Update babel-preset-es2015 to babel-preset-env
    
    * Add plugin to use static properties on classes and update browsers list
    
    * Update with official data

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const moduleExportsPlugin = require('babel-plugin-add-module-exports');
-const babelPresetEs2015 = require('babel-preset-es2015');
+const babelPresetEnv = require('babel-preset-env');
 const babelPresetStage3 = require('babel-preset-stage-3');
 const babelPresetReact = require('babel-preset-react');
+const babelPluginTransformClassProperties = require('babel-plugin-transform-class-properties');
 const babelPluginTransformExportDefault = require('babel-plugin-transform-export-default');
 const babelPluginTransformEs2015ObjectSuper = require('babel-plugin-transform-es2015-object-super');
 const babelPluginTransformFunctionBind = require('babel-plugin-transform-function-bind');
 
+const targets = {
+  node: 8,
+  chrome: 53,
+  edge: 15,
+  explorer: 11,
+  firefox: 52,
+  safari: 9,
+  ucandroid: 1
+};
+
 module.exports = {
   presets: [
-    babelPresetEs2015,
+    babelPresetEnv.default(null, { targets }),
     babelPresetStage3,
     babelPresetReact,
   ],
   plugins: [
     moduleExportsPlugin.default ? moduleExportsPlugin.default : moduleExportsPlugin,
+    babelPluginTransformClassProperties,
     babelPluginTransformEs2015ObjectSuper,
     babelPluginTransformExportDefault,
     babelPluginTransformFunctionBind,
